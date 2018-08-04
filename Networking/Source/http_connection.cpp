1deba85ac6b341c0d813537da8bba8c99f97d3f5
zerdzhong <zerd.zhong@gmail.com>
2018-08-04 16:56:13 +0800

    add http_connection class

---
+++ Networking/Source/http_connection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhongzhendong on 2018/8/4.
+//
+
+#include "http_connection.hpp"
---
+++ Networking/Source/http_connection.hpp
@@ -0,0 +1,14 @@
+//
+// Created by zhongzhendong on 2018/8/4.
+//
+
+#ifndef GRANARY_HTTP_CONNECTION_HPP
+#define GRANARY_HTTP_CONNECTION_HPP
+
+
+class HttpConnection {
+
+};
+
+
+#endif //GRANARY_HTTP_CONNECTION_HPP
