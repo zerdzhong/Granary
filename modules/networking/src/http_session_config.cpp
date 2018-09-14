4dc85cf3af581eb09352e9ae1c196ad9a08274a6
zerdzhong <zerd.zhong@gmail.com>
2018-09-14 23:00:54 +0800

    add session config

--- modules/networking/src/http_session_config.cpp
+++ modules/networking/src/http_session_config.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "http_session_config.hpp"
+
+http_session_config& http_session_config::DefaultSessionConfig() {
+    static http_session_config defaultConfig;
+    return defaultConfig;
+}
+
+void http_session_config::SetRequestTimeout(time_interval request_timeout) {
+    request_timeout_ = request_timeout;
+}
+
+time_interval http_session_config::RequestTimeout(time_interval request_timeout) {
+    return request_timeout_;
+}
+
+http_session_config::http_session_config() {
+
+}
+
+http_session_config::~http_session_config() {
+
+}
