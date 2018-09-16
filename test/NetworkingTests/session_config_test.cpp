b65cd116ea2baa6f654d0b2afcd3993b37288ec6
zerdzhong <zerd.zhong@gmail.com>
2018-09-16 22:13:10 +0800

    session config test case

--- test/NetworkingTests/session_config_test.cpp
+++ test/NetworkingTests/session_config_test.cpp
@@ -14,15 +14,40 @@
 #undef private
 #undef protected
 
+using namespace std;
+
 TEST(SessionConfigTest, Default)
 {
-    http_session_config config = http_session_config::DefaultSessionConfig();
-    ASSERT_NE(nullptr, &config);
+    http_session_config *config = http_session_config::DefaultSessionConfig();
+    ASSERT_NE(nullptr, config);
+    delete config;
 }
 
 TEST(SessionConfigTest, RequestTimeout)
 {
-    http_session_config config = http_session_config::DefaultSessionConfig();
-    config.SetRequestTimeout(1);
-    ASSERT_EQ(config.RequestTimeout(), 1);
+    http_session_config *config = http_session_config::DefaultSessionConfig();
+    config->SetRequestTimeout(1);
+    ASSERT_EQ(config->RequestTimeout(), 1);
+    delete config;
+}
+
+TEST(SessionConfigTest, Configs)
+{
+    http_session_config *config = http_session_config::DefaultSessionConfig();
+    config->SetRequestTimeout(1);
+    ASSERT_EQ(config->RequestTimeout(), 1);
+
+    vector<string> headers = {"header1:1","header2:2"};
+    config->SetAdditionalHeaders(headers);
+    ASSERT_EQ(config->AdditionalHeaders(), headers);
+    ASSERT_EQ(config->AdditionalHeaders().size(), 2);
+
+    map<string, set<string>> url_resolve = {{"www.baidu.com", {"8.8.8.8", "1.1.1.1"}}};
+    config->SetUrlResolve(url_resolve);
+    ASSERT_EQ(config->UrlResolves(), url_resolve);
+    ASSERT_EQ(config->UrlResolves().size(), 1);
+
+    config->AddResolve("www.google.com", {"8.8.8.8", "1.1.1.1"});
+    ASSERT_EQ(config->UrlResolves().size(), 2);
+
 }
