94123417a89a31a9aafed9838135b0839aa6edb9
zerdzhong <zerd.zhong@gmail.com>
2018-10-07 20:56:55 +0800

    travis linux update cmake version to 3.12.2

---
+++ ci/install-linux.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    echo "Not a Linux build; skipping installation"
+    exit 0;
+fi
+
+DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+
+mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+
+travis_retry wget --no-check-certificate https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz
+tar -xvf cmake-3.12.3-Linux-x86_64.tar.gz > /dev/null
+mv cmake-3.3.2-Linux-x86_64 cmake-install
+PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:$PATH
+cd ${TRAVIS_BUILD_DIR}
