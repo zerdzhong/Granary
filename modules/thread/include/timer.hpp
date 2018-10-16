875558281bb7b97ffb4c5a0703f27e5cd6b476be
zerdzhong <zerd.zhong@gmail.com>
2018-10-16 22:44:38 +0800

    add timer define

--- modules/thread/include/timer.hpp
+++ modules/thread/include/timer.hpp
@@ -5,9 +5,22 @@
 #ifndef GRANARY_TIMER_HPP
 #define GRANARY_TIMER_HPP
 
+#include <thread>
+
+class Timer;
+
+typedef double TimeInterval;
+typedef void (*TimerCallBack)(Timer *timer, void *info);
 
 class Timer {
+public:
+    Timer();
+    ~Timer();
 
+private:
+    std::mutex mutex_;
+    TimeInterval  interval_;
+    TimerCallBack callout_;
 };
 
 
