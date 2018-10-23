0260bdfcd66cf6f22ba94108a6d6e07a7e860980
zerdzhong <zerd.zhong@gmail.com>
2018-10-23 22:32:49 +0800

    add thread class

---
+++ modules/thread/include/thread.hpp
@@ -0,0 +1,14 @@
+//
+// Created by zhongzhendong on 2018/10/23.
+//
+
+#ifndef GRANARY_THREAD_HPP
+#define GRANARY_THREAD_HPP
+
+
+class thread {
+
+};
+
+
+#endif //GRANARY_THREAD_HPP
---
+++ modules/thread/src/thread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhongzhendong on 2018/10/23.
+//
+
+#include "thread.hpp"
