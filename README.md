6852f5b28974a98a9e61509345826e672d8e6a08
zerdzhong <zerd.zhong@gmail.com>
2018-11-25 17:55:03 +0800

    add cache module

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Granary [![Build Status](https://travis-ci.com/zerdzhong/Granary.svg?branch=master)](https://travis-ci.com/zerdzhong/Granary)
 
-The project is a cross platform download framework.Use cmake generate build system.
+The project is a cross platform download framework.
+
+## Building
+Use cmake generate build system.
 
 
 
---
+++ modules/cache/mountain.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAXELEMS = 100;
+
+long data[MAXELEMS]
+
+int test(int elems, int stride) {
+    long i, sx2 = stride * 2, sx3 = stride *3, sx4 = stride *4;
+    long acc0 = 0, acc1 = 0, acc2 = 0, acc3 = 0;
+
+    return ((acc0 + acc1) +(acc2 + acc3));
+}
+
+double run(int size, int stride, double Mhz)
+{
+    double cycles;
+
+}
