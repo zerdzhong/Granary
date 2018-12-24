0e132e28e655e165feeffe44859aaa7f5fc5cfff
zerdzhong <zerd.zhong@gmail.com>
2018-12-24 21:54:49 +0800

    add check support type

--- modules/base/checks.hpp
+++ modules/base/checks.hpp
@@ -6,9 +6,79 @@
 #define GRANARY_CHECKS_HPP
 
 
-class checks {
+#include <string>
 
-};
+namespace granary {
+    namespace granary_checks_impl {
+
+        enum class CheckArgType : int {
+            kEnd = 0,
+            kInt,
+            kLong,
+            kLongLong,
+            kUInt,
+            kULong,
+            kULongLong,
+            kDouble,
+            kLongDouble,
+            kCharP,
+            kStdString,
+            kVoidP
+        };
+
+        void FatalLog(const char* file,
+                      int line,
+                      const char* message,
+                      const CheckArgType* fmt,
+                      ...);
+
+        template <CheckArgType A, typename T>
+        struct Val {
+            CheckArgType Type() { return A; }
+            T GetValue() const {return val; }
+            T val;
+        };
+
+        inline Val<CheckArgType::kInt, int> MakeVal(int x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kLong, long> MakeVal(long x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kLongLong, long long> MakeVal(long long x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kUInt, unsigned int> MakeVal(unsigned int x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kULong, unsigned long> MakeVal(unsigned long x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kULongLong, unsigned long long> MakeVal(
+                unsigned long long x) {
+            return {x};
+        }
+
+        inline Val<CheckArgType::kDouble, double> MakeVal(double x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kLongDouble, long double> MakeVal(long double x) {
+            return {x};
+        }
+
+        inline Val<CheckArgType::kCharP, const char*> MakeVal(const char* x) {
+            return {x};
+        }
+        inline Val<CheckArgType::kStdString, const std::string*> MakeVal(
+                const std::string& x) {
+            return {&x};
+        }
+
+        inline Val<CheckArgType::kVoidP, const void*> MakeVal(const void* x) {
+            return {x};
+        }
+    }
+}
 
 
 #endif //GRANARY_CHECKS_HPP
